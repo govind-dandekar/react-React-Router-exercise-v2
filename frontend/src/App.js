cc937e154f32859b27b70b455f55fade8003f80a
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-15 13:01:38 -0800

    add useFetcher to Newslettesignup to interact with action without triggering route transition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -93,7 +93,7 @@ const router = createBrowserRouter([
       {
         path: 'newsletter',
         element: <NewsletterPage />,
-        action: newsletterAction,
+        action: newsletterAction
       },
     ],
   },
--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,36 @@
+import { useEffect } from 'react';
+import { useFetcher } from 'react-router-dom';
+
 import classes from './NewsletterSignup.module.css';
 
 function NewsletterSignup() {
-  return (
-    <form method="post" className={classes.newsletter}>
+  const fetcher = useFetcher();
+	//to interact with action or loader w/o route change, use fetcher
+	const { data, state } = fetcher;
+
+	useEffect(() => {
+		if (state === 'idle' && data && data.message){
+			window.alert(data.message);
+		}
+	}, [data, state])
+	
+	// fetcher.Form triggers an action but does not initialize
+	// a route transition to the page that the loader or 
+	// action belongs to
+	return (
+    <fetcher.Form 
+			method="post" 
+			// trigger newsletter action
+			action="/newsletter"
+			className={classes.newsletter}
+		>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
