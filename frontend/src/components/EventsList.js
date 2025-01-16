cdda524cbcbcb3eb29d0b5c8f726d09eee3b4d3c
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-15 16:35:49 -0800

    manage multiple fetches with data loader, Await, and Suspense

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -9,14 +9,13 @@ function EventsList({ events }) {
   // but not parents routes; can use in components used
   // in an element assigned to a route (children)
   // const events = useLoaderData();
-
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
       <ul className={classes.list}>
         {events.map((event) => (
           <li key={event.id} className={classes.item}>
-            <Link to={event.id}>
+            <Link to={`/events/${event.id}`}>
               <img src={event.image} alt={event.title} />
               <div className={classes.content}>
                 <h2>{event.title}</h2>
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,24 +1,33 @@
-import { useRouteLoaderData, redirect } from 'react-router-dom';
+import { Suspense } from 'react';
+
+import { 
+	useRouteLoaderData, 
+	redirect, 
+	Await } from 'react-router-dom';
+
 import EventItem from '../components/EventItem';
+import EventsList from '../components/EventsList';
 
 function EventDetailPage(){
-	const data = useRouteLoaderData('event-detail');
+	const {event, events} = useRouteLoaderData('event-detail');
 	
 	return <>
-		<EventItem 
-			event={data.event}
-		/>
+		<Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+			<Await resolve={event}>
+				{(loadedEvent) => <EventItem event={loadedEvent}/>}
+			</Await>
+		</Suspense>
+		<Suspense fallback={<p style={{textAlign: 'center'}}>Loading...</p>}>
+			<Await resolve={events}>
+				{(loadedEvents) => <EventsList events={loadedEvents}/>}
+			</Await>
+		</Suspense>
 	</>
 }
 
 export default EventDetailPage;
 
-// RR passes object to loader fx
-// contains req obj and params
-export async function loader({request, params}){
-
-	const id = params.id
-
+async function loadEvent(id){
 	const response = await fetch('http://localhost:8080/events/' + id);
 
 	if (!response.ok){
@@ -26,8 +35,51 @@ export async function loader({request, params}){
 			message: 'Could not fetch details for selected event'
 		}), {status: 500})
 	} else {
-		return response;
+		const resData = await response.json();
+		return resData.event;
 	}
+}
+
+async function loadEvents(){
+	// fetch returns Promise that resolves to a Response
+	// RR supports response objects
+	const response = await fetch('http://localhost:8080/events');
+
+	if (!response.ok) {
+		// 400 or 500 status
+		// return { 
+		// 	isError: true, 
+		// 	message: 'Could not fetch events'
+		// };
+
+		// when Error thrown, RR will render closest errorElement
+		throw new Response(JSON.stringify({
+			message: 'Could not fetch events'
+		}), {status: 500})
+	} else {
+		// RR makes returned data available
+		// can return any kind of data via loader
+		// including new Response() on client-side;
+		// RR will automatically extract data
+		// useLoaderData() automatically gives data that's
+		// part of the response
+		const resData = await response.json();
+		return resData.events;
+	}
+}
+
+// RR passes object to loader fx
+// contains req obj and params
+export async function loader({request, params}){
+	const id = params.id
+
+	return ({
+		// will wait for laodEvent to complete
+		// before loading page component 
+		event: await loadEvent(id),
+		// will loadEvents after page was loaded
+		events: loadEvents()
+	})
 }
 
 export async function action({params, request}){
@@ -45,3 +97,4 @@ export async function action({params, request}){
 		return redirect('/events')
 	}
 }
+
